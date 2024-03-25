18d3f2c297b947bd27c54c2b926ad92d8da6c50b
Allen Soberanes <asoberan@syr.edu>
2024-03-25 13:31:20 -0400

    Fix django urls import

--- api_app/urls.py
+++ api_app/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import url, include
+from django.urls import re_path
+from django.conf.urls import include
 
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import SamplesViews
@@ -9,13 +10,13 @@ from .views import SamplesViews
 
 
 urlpatterns = [
-    url(r'^samples/$', SamplesListViews.as_view()),
-    url(r'^samples/(?P<pk>[0-9]+)/$', SamplesDetailViews.as_view()),
-    url(r'^datafiles/$', DatafilesListViews.as_view()),
-    url(r'^datafiles/(?P<pk>[0-9]+)/$', DatafilesDetailViews.as_view()),
-    url(r'^rest-auth/', include('rest_auth.urls')),
-    url(r'^sampleupload/$', SamplesViews.as_view(), name='sample-upload'),
-    url(r'^datafileupload/$', DatafileViews.as_view(), name='datafile-upload'),
+    re_path(r'^samples/$', SamplesListViews.as_view()),
+    re_path(r'^samples/(?P<pk>[0-9]+)/$', SamplesDetailViews.as_view()),
+    re_path(r'^datafiles/$', DatafilesListViews.as_view()),
+    re_path(r'^datafiles/(?P<pk>[0-9]+)/$', DatafilesDetailViews.as_view()),
+    re_path(r'^rest-auth/', include('rest_auth.urls')),
+    re_path(r'^sampleupload/$', SamplesViews.as_view(), name='sample-upload'),
+    re_path(r'^datafileupload/$', DatafileViews.as_view(), name='datafile-upload'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
